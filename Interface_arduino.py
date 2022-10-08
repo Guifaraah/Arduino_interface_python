f40dded2f017e1a37bf5c1027d4fab5d3cef2190
Guilherme Farah <gdcf.pro@gmail.com>
2022-10-08 17:31:55 -0300

    alteração em algumas variáveis (sem teste)

--- Interface_arduino.py
+++ Interface_arduino.py
@@ -1,38 +1,38 @@
 import serial   # Importando a biblioteca que faz a comunicação entre python e a porta serial
 from tkinter import *  # Importando a biblioteca responsável pela interface gráfica.
 
-valor_led = 1  # Variável para definir apenas o estado inicial dos Leds
-valor_led2 = 1
+led_1 = 1  # Variável para definir apenas o estado inicial dos Leds
+led_2 = 1
 
 arduino = serial.Serial('COM9', 9600)  # Função que está definindo com qual porta serial o código irá se comunicar e o baud rate. Ambos foram definidos no arduino IDE
 
 
-def funcao_botao():
+def botao():
     global valor_led  # Variavel global de valor_led para retornar com o valor = 1 que já foi definido antes
     global arduino
 
-    if valor_led == 1:  # quando o botão é acionado, ele está com o valor "1" atribuído a ele
-        valor_led = 0   # Para que, quando o botão for acionado de novo o estado do led mude, é necessário atribuir outro valor para a variável
-        arduino.write(b'1')  # Quando acionado, o valor '1' é enviado para a porta serial do arduino, fazendo com que a ledVermelha Ligue. Isso está especificado no código no arduino IDE.
+    if led_1 == 1:  # quando o botão é acionado, ele está com o valor "1" atribuído a ele
+        led_1 = 0   # Para que, quando o botão for acionado de novo o estado do led mude, é necessário atribuir outro valor para a variável
+        arduino.write(b'0')  # Quando acionado, o valor '1' é enviado para a porta serial do arduino, fazendo com que a ledVermelha Ligue. Isso está especificado no código no arduino IDE.
         label_estado_led["text"] = "  LIGADO "  # a caixa de texto da interface irá mostrar a palavra "ligado" para representar o estado do led.
         label_estado_led["foreground"] = "black"  # Essa função representa a cor da letra da caixa de texto
     else:
-        valor_led = 1
-        arduino.write(b'0')  # o 'b' é necessário para que o arduino identifique o número em questão em binário, fazendo assim a correta leitura do valor recebido na porta serial.
+        led_1 = 1
+        arduino.write(b'1')  # o 'b' é necessário para que o arduino identifique o número em questão em binário, fazendo assim a correta leitura do valor recebido na porta serial.
         label_estado_led["text"] = "DESLIGADO"
         label_estado_led["foreground"] = "black"
 
-def funcao_botao2():
-    global valor_led2
+def botao_2():
+    global led_2
     global arduino
 
-    if valor_led2 == 1:
-        valor_led2 = 0
+    if led_2 == 1:
+        led_2 = 0
         arduino.write(b'2')
         label_estado_led2["text"] = "  LIGADO "
         label_estado_led2["foreground"] = "black"
     else:
-        valor_led2 = 1
+        led_2 = 1
         arduino.write(b'3')
         label_estado_led2["text"] = "DESLIGADO"
         label_estado_led2["foreground"] = "black"
@@ -47,7 +47,7 @@ label_1.place(x=300,y=50)
 label_estado_led = Label(janela,text="",font="Arial 15",background="red")
 label_estado_led.place(x=360,y=140)
 
-botao_1 = Button(janela, width=20, text="ENVIAR", command=funcao_botao ,background="yellow")
+botao_1 = Button(janela, width=20, text="ENVIAR", command=botao ,background="yellow")
 botao_1.place(x=350, y=300)
 
 janela.geometry("600x400+0+0")
@@ -60,7 +60,7 @@ label_2.place(x=50,y=50)
 label_estado_led2 = Label(janela,text="",font="Arial 15",background="blue")
 label_estado_led2.place(x=70,y=140)
 
-botao_2 = Button(janela, width=20, text="ENVIAR", command=funcao_botao2 ,background="yellow")
+botao_2 = Button(janela, width=20, text="ENVIAR", command=botao_2 ,background="yellow")
 botao_2.place(x=50, y=300)
 
 janela.mainloop()
--- Leds/Leds.ino
+++ Leds/Leds.ino
@@ -1,11 +1,11 @@
-int ledAzul = 8;  //Atribuindo o pino digital 8 do arduino à variável ledAzul
-int ledVermelho = 4;  // Atribuindo o pino digital 4 do arduino à variável ledVermelho
+int led_Azul = 8;  //Atribuindo o pino digital 8 do arduino à variável ledAzul
+int led_Vermelho = 4;  // Atribuindo o pino digital 4 do arduino à variável ledVermelho
 int valor_recebido;  // Criando variável que se refere aos valores recebidos pela porta serial do arduino IDE.
 
 void setup() {
   Serial.begin(9600);  // Baud rate ou velocidade dos dados recebidos.
-  pinMode(ledVermelho, OUTPUT);  // Define que o pino do ledVermelho/ledAzul funcionará como uma saída de sinal
-  pinMode(ledAzul, OUTPUT);
+  pinMode(led_Vermelho, OUTPUT);  // Define que o pino do ledVermelho/ledAzul funcionará como uma saída de sinal
+  pinMode(led_Azul, OUTPUT);
 
 }
 
@@ -14,17 +14,17 @@ void loop() {
   {
     valor_recebido = Serial.read();  // Se a afirmação acima for verdadeira, o valor lido na porta serial será atribuído à variável "valor_recebido"
   }
-  if(valor_recebido == '1'){  
-    digitalWrite(ledVermelho,HIGH);  //ledVermelho liga.
+  if(valor_recebido == '0'){  
+    digitalWrite(led_Vermelho,HIGH);  //ledVermelho liga.
   }
-  else if(valor_recebido == '0'){
-    digitalWrite(ledVermelho,LOW);  //ledVermelho desliga
+  else if(valor_recebido == '1'){
+    digitalWrite(led_Vermelho,LOW);  //ledVermelho desliga
   }
   else if(valor_recebido == '2'){  // ledAzul liga
-    digitalWrite(ledAzul,HIGH);
+    digitalWrite(led_Azul,HIGH);
   }
   else if(valor_recebido == '3'){  //ledAzul desliga
-    digitalWrite(ledAzul,LOW);
+    digitalWrite(led_Azul,LOW);
   }
 
 }
